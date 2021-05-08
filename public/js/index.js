cd8f1ef71900f1542dc492d5420a1c95579fd45c
Kaashyap <kaashyap@acceleron.dev>
2021-05-09 00:44:37 +0530

    add password and validations

---
+++ public/js/index.js
@@ -0,0 +1,52 @@
+const socket = io();
+
+function setAction(form) {
+  let username = document.getElementById('username').value;
+  let room = document.getElementById('room').value;
+  let shouldCreateRoom = document.getElementById('shouldCreateRoom').checked;
+  let password = document.getElementById('password').value;
+
+  if(shouldCreateRoom){
+    room = randomNumber();
+  }
+
+  socket.emit('validate', {username, room, shouldCreateRoom, password}, (response) => {
+
+    if(response === 200){
+
+      // location.href = `chat.html?username=${username}&room=${room}`
+
+      socket.emit('joinRoom', {username, room})
+
+      
+
+    }
+    else if(response === 401){
+
+      alert(401);
+
+    }
+    else if(response === 404){
+
+      alert(404);
+
+    }
+    else if(response === 202){
+
+      // location.href = `chat.html?username=${username}&room=${room}`
+
+      socket.emit('joinRoom', {username, room})
+
+    }
+    else{
+
+      alert("Server Not Responding")
+    }
+  });
+  return false;
+}
+
+function randomNumber(){
+
+  return Date.now();
+}
